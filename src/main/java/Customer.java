5abbfac46f5fe4f79a35717262788ef18e7e716e
ShadowCatP <282276@student.pwr.edu.pl>
2024-05-23 22:16:58 +0300

    fixed the issue with decimal separator

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -1,14 +1,19 @@
 import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
 
 public class Customer {
     private String name;
     private double resistance;
     private double drunkenness;
     private Behavior behavior;
-    private DecimalFormat numberFormat = new DecimalFormat("#.00");
+    private DecimalFormat numberFormat;
     private Beer currentBeer;
 
     public Customer (String name, double resistance, Behavior behavior) {
+        DecimalFormatSymbols symbols = new DecimalFormatSymbols(Locale.getDefault());
+        symbols.setDecimalSeparator('.');
+        numberFormat = new DecimalFormat("#.00", symbols);
         this.name = name;
         this.resistance = Double.parseDouble(numberFormat.format(resistance));
         this.behavior = behavior;
