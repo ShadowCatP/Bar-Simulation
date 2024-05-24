1012eb822263be357545f92af1970c8a55869c0b
ShadowCatP <282276@student.pwr.edu.pl>
2024-05-24 23:09:04 +0300

    CSV writer init

---
+++ src/main/java/CSVWriter.java
@@ -0,0 +1,5 @@
+public class CSVWriter {
+
+    private static final String CSV_HEADER = "Iteration,Customer ID,Customer Type,Beer Type,Drunkenness Level,Resistance Level,Remaining Customers,Return Status";
+
+}
